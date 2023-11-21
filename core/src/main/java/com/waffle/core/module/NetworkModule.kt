8fca799be4992079c1a7c7804a2cd01e4fa8d39c
Enrico Maricar <enrico.maricar@gmail.com>
2023-11-21 07:32:29 +0700

    [enrico]

--- core/src/main/java/com/waffle/core/module/NetworkModule.kt
+++ core/src/main/java/com/waffle/core/module/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.waffle.core.module
 import com.google.gson.GsonBuilder
 import com.waffle.core.api.ApiService
 import io.reactivex.schedulers.Schedulers
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -17,12 +18,19 @@ val networkModule = module{
         val logging = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog=")
+            .add(hostname, "sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HeaderInterceptor())
             .addInterceptor(logging)
             .connectTimeout(60L, TimeUnit.SECONDS)
             .readTimeout(60L, TimeUnit.SECONDS)
             .writeTimeout(60L, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single{
--- core/src/main/java/com/waffle/core/module/RoomModule.kt
+++ core/src/main/java/com/waffle/core/module/RoomModule.kt
@@ -2,12 +2,18 @@ package com.waffle.core.module
 
 import androidx.room.Room
 import com.waffle.core.local.LocalDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val roomModule = module {
     single {
-        Room.databaseBuilder(androidContext(), LocalDatabase::class.java, "db")
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("movieApp".toCharArray())
+        val factory = SupportFactory(passphrase)
+        Room.databaseBuilder(androidContext(), LocalDatabase::class.java, "movie_db")
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 }
